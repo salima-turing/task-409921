96d9387cfbd16375cc3f819a754b0f362a179bf1
salima-turing <salima.o@turing.com>
2024-12-31 11:50:04 +0100

    Create script.js

---
+++ turn1modelA/script.js
@@ -0,0 +1,12 @@
+// Sample JavaScript file
+
+function calculate(a, b) {
+    // This is a sample comment explaining the function
+    return a + b;
+}
+
+console.log(calculate(5, 10)); // Output: 15
+
+function notUsedFunction() {
+    console.log("This will not be used.");
+}
