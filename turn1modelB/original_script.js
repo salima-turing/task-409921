06cb70761af4cb0a966d1ef70d7a7bd06f88dca8
salima-turing <salima.o@turing.com>
2024-12-31 11:54:01 +0100

    Create original_script.js

---
+++ turn1modelB/original_script.js
@@ -0,0 +1,9 @@
+function greetUser(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greetUser("Alice");
+greetUser("Bob");
+
+const aVeryLongVariableNameThatWeWillMangle = "This will be much shorter";
+console.log(aVeryLongVariableNameThatWeWillMangle);
